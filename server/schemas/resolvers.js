05fce4c949523b4b066258a803718198d7fdcce5
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-07-12 13:20:59 -0600

    mutation update

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,26 @@
 const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {},
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
   },
 
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
+      const token = signToken(user);
 
-      return { user };
+      return { token, user };
     },
     login: async (parent, { email, password }) => {},
     saveBook: async (parent, args, context) => {
