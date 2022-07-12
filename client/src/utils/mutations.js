65e60241675d502e21dea6a4e90e36e67529766f
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-07-12 14:29:06 -0600

    front end mutations added

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,59 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($bookId: ID!) {
+    saveBook(savedBook: $bookId) {
+      _id
+      username
+      email
+      savedBooks  {
+        bookId
+        authors
+        description
+        title
+        image
+      }
+    }
+  })`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
